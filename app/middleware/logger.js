c89b50e73c0e5b7527ed0ceb392bdc3294f33094
relzhong <relzhong@outlook.com>
2019-03-21 16:14:22 +0800

    enhance logger

--- app/middleware/logger.js
+++ app/middleware/logger.js
@@ -3,24 +3,31 @@ const path = require('path');
 module.exports = () => {
   const skipExt = [ '.png', '.jpeg', '.jpg', '.ico', '.gif' ];
   return async function(ctx, next) {
-    await ctx.service.authenticate.policyValidate();
+    const ext = path.extname(ctx.url).toLocaleLowerCase();
+    const isSkip = skipExt.indexOf(ext) !== -1 && ctx.status < 400;
+    const ua = ctx.get('user-agent') || '';
+    if (!isSkip) {
+      if (ctx.request.method !== 'GET' && ctx.user) {
+        ctx.logger.info({ 
+          query: ctx.query, 
+          queries: ctx.queries,
+          params: ctx.params,
+          reqBody: ctx.request.body,
+        });
+      } else {
+        ctx.logger.info(ua);
+      }
+    }
+
     await next();
     const rs = ctx.starttime ? Date.now() - ctx.starttime : 0;
     ctx.set('X-Response-Time', rs);
 
-    const ext = path.extname(ctx.url).toLocaleLowerCase();
-    const isSkip = skipExt.indexOf(ext) !== -1 && ctx.status < 400;
-
     if (!isSkip) {
-      const ua = ctx.get('user-agent') || '';
       if (ctx.request.method !== 'GET' && ctx.user) {
-        ctx.logger.info({ query: ctx.query, queries: ctx.queries,
-          params: ctx.params,
-          reqBody: ctx.request.body,
+        ctx.logger.info({ 
           resBody: ctx.response.body instanceof Buffer ? ctx.response.body.toString() : ctx.response.body, // support proxy
         });
-      } else {
-        ctx.logger.info(ua);
       }
     }
   };
